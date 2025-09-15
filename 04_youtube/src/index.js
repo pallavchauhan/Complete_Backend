6884863a2104ae7a4c8d34a8cd5689ee82c0119b
Pallav Chauhan <pallavchauhan2003@gmail.com>
2025-09-15 14:47:58 +0000

    connect to mongo atlas

---
+++ 04_youtube/src/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+
+import connectDB from "./db/index.js";
+
+connectDB()
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+; (async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("ERROR : ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log("app is working fine");
+            
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+ })()
+*/
